d2756899ed64c1a7a900d0d37b175e394484e7a9
嗨吧JanGuo <15315988671@163.com>
2020-06-21 15:59:32 +0800

    Observer Pattern

--- JavaCode/src/main/java/com/janguo/javabasic/concurrent/designpatterns/observer/ThreadLifeCycleObserver.java
+++ JavaCode/src/main/java/com/janguo/javabasic/concurrent/designpatterns/observer/ThreadLifeCycleObserver.java
@@ -19,7 +19,7 @@ public class ThreadLifeCycleObserver implements LifeCycleListener {
                         notifyChange(new RunnableEvent(RunnableState.RUNNING, Thread.currentThread(), null));
                         System.out.println("Query form Database by " + id);
                         Thread.sleep(200);
-//                        int a = 10/0;
+                        // int a = 10 / 0;
                         notifyChange(new RunnableEvent(RunnableState.DONE, Thread.currentThread(), null));
                     } catch (Exception e) {
                         notifyChange(new RunnableEvent(RunnableState.ERROR, Thread.currentThread(), e));
@@ -37,12 +37,20 @@ public class ThreadLifeCycleObserver implements LifeCycleListener {
             }
             if (runnableEvent.getState() == ObservableRunnable.RunnableState.DONE) {
                 System.out.println("Listener Find: Thread" + runnableEvent.getThread().getName() + "运行结束！");
-                System.out.println("准备向数据库中\r上传数据");
+                System.out.println("准备向数据库中---上传数据");
                 System.out.println("Running--------Update To Database!");
             }
             if (runnableEvent.getState() == ObservableRunnable.RunnableState.ERROR) {
                 System.out.println("Listener Find: Thread" + runnableEvent.getThread().getName() + "出现异常，而终止");
+                runnableEvent.getCause().printStackTrace();
             }
+//            System.out.println("Listener Find: Thread" + runnableEvent.getThread().getName() + "正在运行！");
+//            if (runnableEvent.getCause() == null) {
+//                System.out.println("Listener Find: Thread" + runnableEvent.getThread().getName() + "运行结束！");
+//
+//            } else {
+//                runnableEvent.getCause().printStackTrace();
+//            }
 
         }
     }
