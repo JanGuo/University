ef2ea8ba4f85281b5c155697e3c35d03ed51664c
嗨吧JanGuo <15315988671@163.com>
2020-06-25 10:51:38 +0800

    CyclicBarrier

--- JavaCode/src/main/java/com/janguo/javabasic/concurrent/jucutils/cyclicbarrier/CyclicBarrierExample1.java
+++ JavaCode/src/main/java/com/janguo/javabasic/concurrent/jucutils/cyclicbarrier/CyclicBarrierExample1.java
@@ -22,6 +22,9 @@ import java.util.concurrent.TimeUnit;
  * 1. CountDownLatch 不能 reset， 二CyclicBarrier是可以循环使用的额
  * 2. CountDownLatch 工作线程之间不会相互关心， CyclicBarrier 工作线程必须等到同一个共同的点才会继续去执行某个动作
  *          见eg2 构造函数中传入了 Runnable任务
+ *
+ *「1. CountDownLatch的计数器只能使用一次。而CyclicBarrier的计数器可以使用reset() 方法重置。所以CyclicBarrier能处理更为复杂的业务场景，比如如果计算发生错误，可以重置计数器，并让线程们重新执行一次。
+ * 2. CyclicBarrier还提供其他有用的方法，比如getNumberWaiting方法可以获得CyclicBarrier阻塞的线程数量。isBroken方法用来知道阻塞的线程是否被中断。比如以下代码执行完之后会返回true。」
  */
 public class CyclicBarrierExample1 {
     public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
