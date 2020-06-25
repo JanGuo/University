81d262856a9a03fb8b377f55126463e18a3448cb
嗨吧JanGuo <15315988671@163.com>
2020-06-25 10:24:37 +0800

    CyclicBarrier

--- JavaCode/src/main/java/com/janguo/javabasic/concurrent/jucutils/cyclicbarrier/CyclicBarrierExample2.java
+++ JavaCode/src/main/java/com/janguo/javabasic/concurrent/jucutils/cyclicbarrier/CyclicBarrierExample2.java
@@ -1,4 +1,4 @@
-package com.janguo.javabasic.concurrent.jucutils.barrier;
+package com.janguo.javabasic.concurrent.jucutils.cyclicbarrier;
 
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
