e025b33299be8707bffc6a67e17a797744051b14
嗨吧JanGuo <15315988671@163.com>
2020-06-30 23:13:42 +0800

    PriorityBlockingQueue

--- JavaCode/src/main/java/com/janguo/javabasic/concurrent/collectionsqueue/blocking/PriorityBlockingQueueExample.java
+++ JavaCode/src/main/java/com/janguo/javabasic/concurrent/collectionsqueue/blocking/PriorityBlockingQueueExample.java
@@ -3,6 +3,9 @@ package com.janguo.javabasic.concurrent.collectionsqueue.blocking;
 import java.util.Comparator;
 import java.util.concurrent.PriorityBlockingQueue;
 
+/**
+ * 底层数组 无边界
+ */
 public class PriorityBlockingQueueExample {
     public <T> PriorityBlockingQueue<T> creat() {
         return new PriorityBlockingQueue<T>();
