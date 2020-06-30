b7f87905e317ff510b8d262b0cdc93c24fca8f03
嗨吧JanGuo <15315988671@163.com>
2020-06-30 23:13:15 +0800

    ArrayBockingQueue

--- JavaCode/src/main/java/com/janguo/javabasic/concurrent/collectionsqueue/blocking/ArrayBockingQueueExample1.java
+++ JavaCode/src/main/java/com/janguo/javabasic/concurrent/collectionsqueue/blocking/ArrayBockingQueueExample1.java
@@ -3,6 +3,9 @@ package com.janguo.javabasic.concurrent.collectionsqueue.blocking;
 
 import java.util.concurrent.ArrayBlockingQueue;
 
+/**.
+ *  底层数组 有边界
+ */
 public class ArrayBockingQueueExample1 {
 
         public <T> ArrayBlockingQueue<T> creat(int size){
