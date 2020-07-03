bcbc43796792aefd2087b82c6ccb5985cfdac451
嗨吧JanGuo <15315988671@163.com>
2020-07-03 10:57:58 +0800

    Instant

---
+++ JavaCode/src/main/java/com/janguo/javabasic/java8/date/Java8InstantTest.java
@@ -0,0 +1,15 @@
+package com.janguo.javabasic.java8.date;
+
+import java.time.Instant;
+
+/**
+ * Instant 时间线
+ *
+ * 不带时区的 -- UTC
+ * 2020-07-03T02:57:00.583Z
+ */
+public class Java8InstantTest {
+    public static void main(String[] args) {
+        System.out.println(Instant.now());
+    }
+}
