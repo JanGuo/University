70ce1ab53b2901434c8d56070ec5e4e02bdb4660
嗨吧JanGuo <15315988671@163.com>
2020-07-06 16:53:04 +0800

    leetcode

---
+++ LeetCodePractice/src/main/java/com/janguo/leetcode/editor/cn/TwoSum.java
@@ -0,0 +1,50 @@
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。 
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。 
+//
+// 
+//
+// 示例: 
+//
+// 给定 nums = [2, 7, 11, 15], target = 9
+//
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+// 
+// Related Topics 数组 哈希表 
+// 👍 8583 👎 0
+
+
+package com.janguo.leetcode.editor.cn;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        Solution solution = new TwoSum().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                map.put(nums[i], i);
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                int findI = target - nums[i];
+                Integer integer = map.get(findI);
+                if (map.containsKey(findI) && integer != i) {
+                    return new int[]{i, integer};
+                }
+            }
+            throw new IllegalArgumentException("No two sum solution");
+        }
+
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
