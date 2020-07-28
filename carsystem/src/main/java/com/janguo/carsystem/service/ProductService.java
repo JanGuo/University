4cf39d2a37ad4df70a4cd40d45c030b18877ca61
嗨吧JanGuo <15315988671@163.com>
2020-07-28 21:48:13 +0800

    add productService

--- carsystem/src/main/java/com/janguo/carsystem/service/ProductService.java
+++ carsystem/src/main/java/com/janguo/carsystem/service/ProductService.java
@@ -1,4 +1,16 @@
 package com.janguo.carsystem.service;
 
+import com.janguo.carsystem.domain.ProductEntity;
+
+import java.sql.Timestamp;
+import java.util.List;
+
 public interface ProductService {
+    List<ProductEntity> getAllProduct();
+    boolean addProduct(ProductEntity product);
+    ProductEntity getProductById(String id);
+    ProductEntity getProductByName(String name);
+    List<ProductEntity> getAllPriceUpValue(Integer price);
+    List<ProductEntity> getAllProductTimeDownValue(Timestamp timestamp);
+
 }
--- carsystem/src/main/java/com/janguo/carsystem/service/impl/ProductServiceImpl.java
+++ carsystem/src/main/java/com/janguo/carsystem/service/impl/ProductServiceImpl.java
@@ -1,4 +1,49 @@
 package com.janguo.carsystem.service.impl;
 
-public class ProductServiceImpl {
+import com.janguo.carsystem.dao.ProductDao;
+import com.janguo.carsystem.domain.ProductEntity;
+import com.janguo.carsystem.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Resource
+    ProductDao productDao;
+
+    @Override
+    public List<ProductEntity> getAllProduct() {
+        return productDao.getAllProduct();
+    }
+
+    @Override
+    public boolean addProduct(ProductEntity product) {
+        return productDao.addProduct(product);
+    }
+
+    @Override
+    public ProductEntity getProductById(String id) {
+        return productDao.getProductById(id);
+    }
+
+    @Override
+    public ProductEntity getProductByName(String name) {
+        return productDao.getProductByName(name);
+    }
+
+    @Override
+    public List<ProductEntity> getAllPriceUpValue(Integer price) {
+        return productDao.getAllPriceUpValue(price);
+    }
+
+    @Override
+    public List<ProductEntity> getAllProductTimeDownValue(Timestamp timestamp) {
+        return productDao.getAllProductTimeDownValue(timestamp);
+    }
+
+
 }
