faaec53da85a00de4efd8501e59a6f04ffbcd3bd
嗨吧JanGuo <15315988671@163.com>
2020-07-30 22:39:40 +0800

    add Supplier Dao

--- carsystem/src/main/java/com/janguo/carsystem/dao/SupplierDao.java
+++ carsystem/src/main/java/com/janguo/carsystem/dao/SupplierDao.java
@@ -1,43 +1,27 @@
 package com.janguo.carsystem.dao;
 
-import com.janguo.carsystem.domain.VictualerEntity;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
+import com.janguo.carsystem.domain.SupplierEntity;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
-public interface VictualerDao {
-    /**
-     *     // 供应商编号
-     *     private String providerId;
-     *     // 供应商名称
-     *     private String victualerName;
-     *     // 供应商地址
-     *     private String victualerAddress;
-     *     // 供应商电话
-     *     private String victualerTel;
-     *     // 供应商开户行编号
-     *     private String victualerBank;
-     *     // 供应商账号
-     *     private String victualerBankId;
-     *     // 供应商备注
-     *     private String victualerRemark;
-     * @return
-     */
-    @Results(id = "victualer",value = {
-            @Result(property = "victualerId",column = "victualer_id"),
-            @Result(property = "victualerName",column = "victualer_name"),
-            @Result(property = "victualerAddress",column = "victualer_address"),
-            @Result(property = "victualerTel",column = "victualer_tel"),
-            @Result(property = "victualerBank",column = "victualer_bank"),
-            @Result(property = "victualerBankId",column = "victualer_bank_id"),
-            @Result(property = "victualerRemark",column = "victualer_remark")
-    })
-    @Select("select * from victualer")
-    List<VictualerEntity> getAllVictualer();
+public interface SupplierDao {
 
-    boolean addVictualer();
+    @Results(id = "supplier",value = {
+            @Result(property = "supplierId",column = "supplier_id"), // 供应商编号
+            @Result(property = "supplierName",column = "supplier_name"),// 供应商名称
+            @Result(property = "supplierAddress",column = "supplier_address"),// 供应商地址
+            @Result(property = "supplierTel",column = "supplier_tel"),// 供应商电话
+            @Result(property = "supplierBank",column = "supplier_bank"),// 供应商开户行编号
+            @Result(property = "supplierBankId",column = "supplier_bank_id"),// 供应商账号
+            @Result(property = "supplierRemark",column = "supplier_remark")// 供应商备注
+    })
+    @Select("select * from supplier")
+    List<SupplierEntity> getAllSupplier();
+
+    @ResultMap("supplier")
+    @Insert("insert into supplier(supplier_id,supplier_name,supplier_address,supplier_tel,supplier_bank,supplier_bank_id,supplier_remark)" +
+            "values(#{supplierId},#{supplierName},#{supplierAddress},#{supplierTel},#{supplierBank},#{supplierBankId},#{supplierRemark})")
+    boolean addSupplier(SupplierEntity supplier);
 }
