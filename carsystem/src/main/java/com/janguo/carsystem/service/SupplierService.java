88bac222610b234653860467b34c8ac245714809
嗨吧JanGuo <15315988671@163.com>
2020-07-30 22:40:08 +0800

    add Suppiler Service

--- carsystem/src/main/java/com/janguo/carsystem/service/SupplierService.java
+++ carsystem/src/main/java/com/janguo/carsystem/service/SupplierService.java
@@ -1,4 +1,10 @@
 package com.janguo.carsystem.service;
 
+import com.janguo.carsystem.domain.SupplierEntity;
+
+import java.util.List;
+
 public interface SupplierService {
+    List<SupplierEntity> getAllSupplier();
+    boolean addSupplier(SupplierEntity supplier);
 }
--- carsystem/src/main/java/com/janguo/carsystem/service/impl/SupplierServiceImpl.java
+++ carsystem/src/main/java/com/janguo/carsystem/service/impl/SupplierServiceImpl.java
@@ -1,4 +1,25 @@
 package com.janguo.carsystem.service.impl;
 
-public class SupplierServiceImpl {
+import com.janguo.carsystem.dao.SupplierDao;
+import com.janguo.carsystem.domain.SupplierEntity;
+import com.janguo.carsystem.service.SupplierService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class SupplierServiceImpl implements SupplierService {
+
+    @Resource
+    SupplierDao supplierDao;
+    @Override
+    public List<SupplierEntity> getAllSupplier() {
+        return supplierDao.getAllSupplier();
+    }
+
+    @Override
+    public boolean addSupplier(SupplierEntity supplier) {
+        return supplierDao.addSupplier(supplier);
+    }
 }
