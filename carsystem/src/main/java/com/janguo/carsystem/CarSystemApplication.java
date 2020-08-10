f123615954a88270c2589a4d1c796e26bfb47bec
嗨吧JanGuo <15315988671@163.com>
2020-08-10 22:25:57 +0800

    rename MainClass- Name

--- carsystem/src/main/java/com/janguo/carsystem/CarSystemApplication.java
+++ carsystem/src/main/java/com/janguo/carsystem/CarSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarsystemApplication {
+public class CarSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CarsystemApplication.class, args);
+        SpringApplication.run(CarSystemApplication.class, args);
     }
 
 }
